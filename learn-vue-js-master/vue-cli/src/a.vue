02ea0fca44be585c307cd4f5369df3afa40c63f6
SON_JIYOUNG <djaaksmscjs@radcns.com>
2022-09-21 11:09:41 +0900

    1. 기초 문법 강의

---
+++ learn-vue-js-master/vue-cli/src/a.vue
@@ -0,0 +1,32 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+// javascript
+methods: {
+    addNum: function() {
+        
+    }
+}
+}
+</script>
+
+<style>
+/* css */
+</style>
